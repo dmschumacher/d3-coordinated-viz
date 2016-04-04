a8c283a4125439355e40486775d0a66fa1ccd238
Dan Schumacher <dmschumacher@wisc.edu>
2016-04-04 17:27:44 -0500

    Module 9 assignment completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 (function(){
 
 
-    var attrArray = ["CarAlone","Carpool","Public","WalkedOrBiked","TaxiMotorOther","WorkedAtHome","InState","InCounty","OutCounty","OutState","Time"];
+    var attrArray = ["TotWorkers", "DroveAlone", "twoCarpool", "threeCarpool", "fourCarpool", "PublicTrans", "Walked", "Bike", "TaxiMotoOther", "WorkedFromHome", "InState", "InCounty", "OutCounty", "OutState", "TotCommuters", "124", "5", "6", "7", "8", "912", "lt10", "1019", "2029", "3044", "4559", "60plus", "MeanTravel"];
+    var motArray = ["DroveAlone", "twoCarpool", "threeCarpool", "fourCarpool", "PublicTrans", "Walked", "Bike", "TaxiMotoOther"];
     var expressed = attrArray[0];
-    console.log("Attribute: " + expressed);
 
     window.onload = setMap();
 
@@ -12,8 +12,8 @@
 
 
         //map frame dimensions
-        var width = window.innerWidth * 0.5,
-            height = 600;
+        var width = window.innerWidth * 0.3,
+            height = window.innerHeight * 0.4;
 
         //create new svg container for the map
         var map = d3.select("body")
@@ -27,7 +27,7 @@
             .center([0, 46])
             .rotate([93, 0, 0])
             .parallels([40, 55])
-            .scale(5000)
+            .scale(2000)
             .translate([width / 2, height / 2]);
 
         //path generator
@@ -36,7 +36,7 @@
 
         //use queue.js to parallelize asynchronous data loading
         d3_queue.queue()
-            .defer(d3.csv, "data/CommuterInfoFinal.csv") //load attributes from csv
+            .defer(d3.csv, "data/CommuterInfoFinal4.csv") //load attributes from csv
             .defer(d3.json, "data/US_CAN_2.topojson") //load background spatial data
             .defer(d3.json, "data/MN_WI_2.topojson")  //load choropleth spatial data
             .await(callback);
@@ -53,7 +53,6 @@
                 .attr("class", "us_canada")
                 .attr("d", path);
 
-
             minnwisc = joinData(minnwisc, csvData);
 
             var colorScale = makeColorScale(csvData);
@@ -61,28 +60,23 @@
             setEnumerationUnits(minnwisc, map, path, colorScale);
 
             setChart(csvData, colorScale);
-        };
 
+        };
 
     };
 
     function joinData(minnwisc, csvData){
 
-        for (var i = 0; i < csvData.length; i++){
+        for (var i = 0; i < csvData.length; i++){//for each county
 
             var csvCounty = csvData[i];
             var csvKey = csvCounty.Id2;
 
-            for (var  j = 0; j < minnwisc.length; j++){
-
-
+            for (var  j = 0; j < minnwisc.length; j++){//pair it with it's corresponding topojson geography
 
                 var props = minnwisc[j].properties;
                 var key = props.GEOID;
-                // console.log("csvKey = " + csvKey);
-                // console.log("key = " + key);
-                // console.log();
-
+ 
                 if (key == csvKey){
                     attrArray.forEach(function(attr){
                         var val = parseFloat(csvCounty[attr]);
@@ -91,7 +85,6 @@
                     })
                 }
             }
-
         }
 
         return minnwisc;
@@ -126,11 +119,11 @@
 
     function makeColorScale(data){
         var colorClasses = [
-            "#f6eff7",
-            "#bdc9e1",
-            "#67a9cf",
-            "#1c9099",
-            "#016c59"
+            "#ffffcc",
+            "#a1dab4",
+            "#41b6c4",
+            "#2c7fb8",
+            "#253494"  
         ];
 
         var colorScale = d3.scale.quantile()
@@ -149,8 +142,15 @@
     };
 
     function setChart(csvData, colorScale){
-        var chartWidth = window.innerWidth * 0.425;
-        var chartHeight = 460;
+
+        var chartWidth = window.innerWidth ,
+            chartHeight = window.innerHeight * 0.5,
+            leftPadding = 100,
+            rightPadding = 5,
+            topBottomPadding = 10,
+            chartInnerWidth = chartWidth - leftPadding - rightPadding,
+            chartInnerHeight = chartHeight - topBottomPadding * 2,
+            translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
         var chart = d3.select("body")
             .append("svg")
@@ -158,33 +158,61 @@
             .attr("height", chartHeight)
             .attr("class", "chart");
 
-        var yScale = d3.scale.linear()
-            .range([0, chartHeight])
-            .domain([0,105]);
+        var chartBackground = chart.append("rect")
+            .attr("class", "chartBackground")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+
+        var yScale = d3.scale.sqrt()
+            .range([chartInnerHeight, 0])
+            .domain([0,650000]);
 
         var bars = chart.selectAll(".bars")
             .data(csvData)
             .enter()
             .append("rect")
             .sort(function(a, b){
-                return a[expressed]-b[expressed]
+                return b[expressed]-a[expressed]
             })
             .attr("class", function(d){
                 return "bars " + d.Id2;
             })
-            .attr("width", chartWidth / csvData.length - 1)
+            .attr("width", chartInnerWidth / csvData.length - 1)
             .attr("x", function(d, i){
-                return i * (chartWidth / csvData.length);
+                return i * (chartInnerWidth / csvData.length) + leftPadding;
             })
             .attr("height", function(d){
-            return yScale(parseFloat(d[expressed]));
+                return chartInnerHeight - yScale(parseFloat(d[expressed]));
             })
             .attr("y", function(d){
-                return chartHeight - yScale(parseFloat(d[expressed]));
+                return yScale(parseFloat(d[expressed])) + topBottomPadding;
             })
                 .style("fill", function(d){
                 return choropleth(d, colorScale);
             });
+
+         var chartTitle = chart.append("text")
+            .attr("x", 200)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text("Number of Variable '" + expressed + "' in each county");
+
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left");
+
+        var axis = chart.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate)
+            .call(yAxis);
+
+        //create frame for chart border
+        var chartFrame = chart.append("rect")
+            .attr("class", "chartFrame")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
 
     };
 
